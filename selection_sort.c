ad88fc52de276c9ec65fe67f96db173929bdcf70
root <root@myvm19>
2021-07-30 12:37:36 +0000

    selection sort

---
+++ selection_sort.c
@@ -0,0 +1,98 @@
+/*
+ * Selection Sort 
+ *
+ * @Prerna(1910990964)
+ * 25/7/2021
+ * 
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/time.h>
+#include<time.h>
+
+
+void selection_sort(int n , int *arr) {
+    for(int i = 0; i < n - 1; i++) {
+        int min_index = i; 
+        int min_element = arr[i];
+        for(int j = i + 1; j < n; j++) {
+            //finding the minimum in the remaining array.
+            if(arr[j] < min_element) {
+                min_index = j;
+                min_element = arr[j];
+            }
+        }
+        //swaping minimum index with ith element
+        if(min_index != i){
+            int temp = arr[min_index];
+            arr[min_index] = arr[i];
+            arr[i] = temp;
+        }
+        //printf("%d ",arr[i]);
+    }
+}
+
+void mixed (int n , int *arr) {
+    int upper= n ;
+    int lower = 1;
+    for(int i = 0; i < n; i++) {
+    	arr[i] = (rand() % (upper - lower + 1)) + lower; // setting array elements to a random value;
+    }
+} 
+
+
+void ascending (int n , int *arr) {
+
+    for(int i = 0; i < n; i++) {
+    	arr[i] = i; //setting array elements in ascending order 
+    }
+}
+
+void descending (int n , int *arr) {
+    int index = 0;
+    for(int i = n-1; index < n; i--) {
+    	arr[index] = i;//setting array elements in descending order
+	    //printf("%d ",arr[index]);
+	    index++;
+	
+    }
+}
+
+
+void Time(int n , int arr[]) {
+    clock_t start = clock();
+    selection_sort(n,arr);//bubble sort
+    clock_t stop = clock();
+    double elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
+    printf("Time in ms: %f\n", elapsed);
+}
+
+
+int main() {
+    int n;
+    scanf("%d",&n);
+
+    int arr[n];
+    
+
+    printf("Random\n");
+    mixed (n , arr); // setting array elements randomly
+    Time(n , arr);
+
+    
+    printf("Ascending\n");
+    ascending (n , arr); // setting array elements in ascending order
+    Time(n , arr);
+
+
+    
+    
+    printf("Descending\n");
+    descending(n , arr); // setting array elements in descending order
+    Time(n , arr);
+    
+    printf("\n");
+    
+    return 0;
+}
